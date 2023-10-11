20df5343ce459dc7fb7d4ab97c38af77fec80295
RaduAlex18 <116818618+RaduAlex18@users.noreply.github.com>
2023-10-11 14:14:24 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercitiul_1
+{
+    internal class Student  
+    {
+        public  string Nume { get; set; }
+
+        public string Prenume { get; set; }
+
+        public int Varsta { get; set; }
+
+        public string Nationalitate { get; set; }
+
+        public List<Materie> Materii { get; set; }
+
+        public Student(string nume, string prenume, int varsta, string nationalitate) 
+        {
+            Nume = nume;    
+            Prenume = prenume;  
+            Varsta = varsta;    
+            Nationalitate = nationalitate;
+            Materii = new List<Materie>();
+        }
+
+        public void Afisare()
+        {
+            Console.WriteLine($"Student:{Nume} {Prenume}, Varsta:{Varsta}, Nationalitate:{Nationalitate}");
+
+            foreach (Materie materie in Materii)
+            {
+                Console.WriteLine($"Titlu:{materie.Titlu}, Durata:{materie.Durata}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using exercitiul_1;
+///*
+///Creati o clasa Student cu nume, prenume, etc si o alta clasa Materie cu titlu, 
+///durata (string). Clasa student o sa aibe o Lista de 
+///materii: public List<Materie> Materii. Faceti o lista de studenti cu cateva 
+///materii si afisati-le.
+///*
+///
+
+
+Materie materie1 = new Materie("Mate", "1 ora");
+Materie materie2 = new Materie("Info", "3 ore");
+Materie materie3 = new Materie("Tic", "2 ore");
+
+
+Student student1 = new Student("Alex", "Mihai", 12, "Roman");
+student1.Materii.Add(materie1);
+student1.Materii.Add(materie2);
+
+Student student2 = new Student("Ion", "Constantin", 82, "Francez");
+student2.Materii.Add(materie3);
+student2.Materii.Add(materie2);
+
+student1.Afisare();
+Console.WriteLine();
+student2.Afisare();
+
