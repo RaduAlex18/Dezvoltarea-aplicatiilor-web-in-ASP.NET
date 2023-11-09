a8c8250059193953e6f65a86db313ed7c9064e71
RaduAlex18 <116818618+RaduAlex18@users.noreply.github.com>
2023-11-09 21:37:37 +0200

    Add files via upload

---
+++ lab_5/tabel1.cs
@@ -0,0 +1,16 @@
+﻿using Lab4_23.Models.Base;
+
+namespace Lab4_23.Models.One_to_Many_my_example_
+{
+    public class tabel1 : BaseEntity
+    {
+        public string? User1 { get; set; }
+
+        public int age1 { get; set; }
+
+
+        public ICollection<tabel2>? tabels2 { get; set; }
+    }
+}
+
+
---
+++ lab_5/tabel2.cs
@@ -0,0 +1,15 @@
+﻿using Lab4_23.Models.Base;
+
+namespace Lab4_23.Models.One_to_Many_my_example_
+{
+    public class tabel2 : BaseEntity
+    {
+        public string User2 { get; set; }
+
+        public int age2 { get; set; }
+
+        public tabel1 tabel1 { get; set; } 
+        public Guid tabel1ID { get; set; }
+    }
+}
+
