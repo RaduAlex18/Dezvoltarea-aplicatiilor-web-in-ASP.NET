b9a8883d09934b7ca780bab0bc4e52f5b88c27ed
RaduAlex18 <116818618+RaduAlex18@users.noreply.github.com>
2023-12-07 21:24:59 +0200

    Add files via upload

---
+++ lab_8/route2.component.ts
@@ -0,0 +1,25 @@
+import { Component , OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-route2',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule],
+  templateUrl: './route2.component.html',
+  styleUrl: './route2.component.scss'
+})
+export class Route2Component implements OnInit {
+
+  public word = ['start', 'merge (am luat din lista cuvantul dupa ce am apasat pe buton)'];
+  public displayValue: string = '';
+
+  ngOnInit() {
+    this.displayValue = this.word[0];
+  }
+
+  buttonGet() {
+    this.displayValue = this.word[1];
+  }
+}
